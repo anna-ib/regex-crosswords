0a2f24babb9d48dbf57228281c1f31f75f7ee863
ekocka <annbtln@gmail.com>
2020-01-01 19:16:50 +0100

    added some generators

--- app/src/main/java/com/example/regexps/CrosswordGenerator.java
+++ app/src/main/java/com/example/regexps/CrosswordGenerator.java
@@ -24,22 +24,14 @@ class CrosswordGenerator {
             new Generator() { public void generate(String str) { generateFakeCharAndQuantifier(str); }},
             new Generator() { public void generate(String str) { generateCharAndQuantifier(str); }},
             new Generator() { public void generate(String str) { generateString(str); }},
-            //new Generator() { public void generate(String str) { generateGroupAndQuantifier(str); }},
-            //new Generator() { public void generate(String str) { generateIntervalAndQuantifier(str); }},
+            new Generator() { public void generate(String str) { generateGroupAndQuantifier(str); }},
+            new Generator() { public void generate(String str) { generateIntervalAndQuantifier(str); }},
             new Generator() { public void generate(String str) { generateAlternation(str); }}
     };
 
     CrosswordGenerator(int width, int height) {
         if (width > 0) this.width = width;
         if (height > 0) this.height = height;
-    }
-
-    String[] getCrosswordHorizontal() {
-        return reCrosswordHorizontal;
-    }
-
-    String[] getCrosswordVertical() {
-        return reCrosswordVertical;
     }
 
     private String generateRe(String str) {
@@ -58,17 +50,114 @@ class CrosswordGenerator {
     }
 
     private void generateGroupAndQuantifier(String str) {
-        generateGroup(str);
+        char a;
+        boolean negate = random.nextBoolean();
+        if (negate) {
+            this.reg.append("[^");
+            for (int i = 0; i < random.nextInt(4) + 1; i++) {
+                a = this.alphabet.charAt(random.nextInt(62));
+                if (str.indexOf(a) == -1) this.reg.append(a);
+                else i--;
+            }
+        }
+        else {
+            StringBuilder string = new StringBuilder(str);
+            int index;
+            this.reg.append("[");
+            for (int i = 0; i < str.length(); i++) {
+                index = random.nextInt(string.length());
+                this.reg.append(string.charAt(index));
+                string.deleteCharAt(index);
+                if (random.nextBoolean()) {
+                    a = this.alphabet.charAt(random.nextInt(62));
+                    if (str.indexOf(a) == -1) this.reg.append(a);
+                }
+            }
+        }
+        this.reg.append("]");
+
         generateQuantifier(str.length());
     }
 
     private void generateIntervalAndQuantifier(String str) {
-        generateInterval(str);
+        int[] charIndexes = new int[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            charIndexes[i] = this.alphabet.indexOf(str.charAt(i));
+            for (int j = i; j > 0; j--) {
+                if (charIndexes[j] < charIndexes[j-1]) {
+                    charIndexes[j] += charIndexes[j-1];
+                    charIndexes[j-1] = charIndexes[j] - charIndexes[j-1];
+                    charIndexes[j] -= charIndexes[j-1];
+                }
+                else break;
+            }
+        }
+        int maxCharIndex = -1, minCharIndex = -1;
+        boolean negate = random.nextBoolean();
+        if (negate) {
+             this.reg.append("[^");
+             int index = random.nextInt(str.length() + 1);
+
+             while(minCharIndex == maxCharIndex || maxCharIndex - minCharIndex == 1) {
+                 if (index == 0) {
+                     minCharIndex = 0;
+                     maxCharIndex = charIndexes[0];
+                 } else if (index == str.length()) {
+                     minCharIndex = charIndexes[index - 1];
+                     maxCharIndex = 61;
+                 } else {
+                     minCharIndex = charIndexes[index - 1];
+                     maxCharIndex = charIndexes[index];
+                 }
+                 if (++index > str.length()) index = 0;
+             }
+
+             minCharIndex++;
+             maxCharIndex--;
+        }
+        else {
+            this.reg.append("[");
+            minCharIndex = charIndexes[0];
+            maxCharIndex = charIndexes[str.length() - 1];
+            minCharIndex = minCharIndex - random.nextInt(minCharIndex + 1);
+            maxCharIndex = maxCharIndex + random.nextInt(62 - maxCharIndex + 1);
+        }
+
+        this.reg.append(this.alphabet.charAt(minCharIndex));
+        if (minCharIndex != maxCharIndex) {
+            if (minCharIndex < 25) {
+                this.reg.append("-");
+                if (maxCharIndex < 26) this.reg.append(this.alphabet.charAt(maxCharIndex));
+                else if (maxCharIndex == 26) this.reg.append("zA");
+                else if (maxCharIndex < 52) this.reg.append("zA-").append(this.alphabet.charAt(maxCharIndex));
+                else if (maxCharIndex == 52) this.reg.append("zA-Z_0");
+                else this.reg.append("zA-Z_0-").append(this.alphabet.charAt(maxCharIndex));
+            }
+            else if (minCharIndex == 25) {
+                if (maxCharIndex == 26) this.reg.append("A");
+                if (maxCharIndex < 52) this.reg.append("A-").append(this.alphabet.charAt(maxCharIndex));
+                else if (maxCharIndex == 52) this.reg.append("A-Z_0");
+                else this.reg.append("A-Z_0-").append(this.alphabet.charAt(maxCharIndex));
+            }
+            else if (minCharIndex < 51) {
+                this.reg.append("-");
+                if (maxCharIndex < 52) this.reg.append(this.alphabet.charAt(maxCharIndex));
+                else if (maxCharIndex == 52) this.reg.append("Z_0");
+                else this.reg.append("Z_0-").append(this.alphabet.charAt(maxCharIndex));
+            }
+            else if (minCharIndex == 51) {
+                if (maxCharIndex == 52) this.reg.append("_0");
+                else this.reg.append("_0-").append(this.alphabet.charAt(maxCharIndex));
+            }
+            else this.reg.append("-").append(this.alphabet.charAt(maxCharIndex));
+        }
+
+        this.reg.append("]");
         generateQuantifier(str.length());
     }
 
     private void generateFakeCharAndQuantifier(String str) {
-        this.reg.append(this.alphabet.charAt(random.nextInt(61)));
+        this.reg.append(this.alphabet.charAt(random.nextInt(62)));
         generateQuantifier(0);
         generators[random.nextInt(generators.length - 1) + 1].generate(str);
     }
@@ -84,14 +173,10 @@ class CrosswordGenerator {
         this.reg.append(")");
     }
 
-    private void generateGroup(String str) {}
-
-    private void generateInterval(String str) {}
-
     private void generateQuantifier(int count) {
-        final String[] quantifiers = {"", "+", "*", "{" + count + "}", "{" + (count - random.nextInt(count + 1)) + ",}", "{" + (count - random.nextInt(count + 1)) + "," + (count + random.nextInt(count + 1)) + "}", "?"};
+        final String[] quantifiers = {"", "+", "{" + count + "}", "{" + (count - random.nextInt(count + 1)) + "," + (count + random.nextInt(count + 1) + 1) + "}", "*", "{" + (count - random.nextInt(count + 1)) + ",}", "?"};
         switch (count) {
-            case 0: { this.reg.append(quantifiers[random.nextInt(quantifiers.length - 2) + 2]); break; }
+            case 0: { this.reg.append(quantifiers[random.nextInt(quantifiers.length - 4) + 4]); break; }
             case 1: { this.reg.append(quantifiers[random.nextInt(quantifiers.length)]); break; }
             default: { this.reg.append(quantifiers[random.nextInt(quantifiers.length - 2) + 1]); break; }
         }
@@ -118,10 +203,14 @@ class CrosswordGenerator {
     private String generateWord(int len) {
         StringBuilder str = new StringBuilder();
         for (int i = 0; i < len; i++){
-            str.append(this.alphabet.charAt(random.nextInt(61)));
+            str.append(this.alphabet.charAt(random.nextInt(62)));
         }
         return str.toString();
     }
+
+    String[] getCrosswordHorizontal() { return reCrosswordHorizontal; }
+
+    String[] getCrosswordVertical() { return reCrosswordVertical; }
 
     int getWidth() { return this.width; }
 
